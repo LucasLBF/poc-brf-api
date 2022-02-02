07be83e83e1b4a72a4e6009c7eefbe790574ef6d
LucasLBF <lucas.fernandes1454@gmail.com>
2022-02-02 11:13:58 -0300

    chore: delete template directory

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, send_from_directory
+from flask import send_from_directory
 from flask import request, jsonify
 from app import pdf_parser
 from os import path
@@ -6,12 +6,10 @@ from os import path
 def init_app(app):
     @app.route("/", methods=["POST"])
     def home():
-        if request.method == "POST":
-            search_term = request.json["kwrd"]
-            results = pdf_parser.parse_pdfs(search_term)
-            return jsonify(results)
+        search_term = request.json["kwrd"]
+        results = pdf_parser.parse_pdfs(search_term)
+        return jsonify(results)
 
-        return render_template("search.html")
 
     @app.route("/uploads/<filename>")
     def upload(filename):
